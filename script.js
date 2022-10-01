0a516cb5fb2588a69e62fc66fa1cb0a28df06291
Robin Reyes <robinreyes.code@gmail.com>
2022-09-30 20:33:45 -0700

    adding a bit of gsap to this

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const hamburger = document.querySelector("svg");
-const links = document.querySelector(".links");
+const burger = document.querySelector("nav svg");
 
-hamburger.addEventListener("click", () => {
-  links.style.transform = "translateX(0%)";
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0, y: 0 },
+      { opacity: 1, delay: 0.5, stagger: 0.25, y: 10 }
+    );
+  }
+  burger.classList.toggle("active");
 });
